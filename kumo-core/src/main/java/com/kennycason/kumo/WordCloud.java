5e77c6fc660dba844a08e6d43f70369724945cf2
Jörg Rech <joerg.rech@gmail.com>
2018-10-14 19:20:50 +0200

    Trying to always log - other project might not using dependency

--- kumo-core/src/main/java/com/kennycason/kumo/WordCloud.java
+++ kumo-core/src/main/java/com/kennycason/kumo/WordCloud.java
@@ -168,7 +168,8 @@ public class WordCloud {
 
         final int maxRadius = dimension.width;
         final int stepFactor = Math.max(1, (int) (maxRadius / 400) ); // Used to speedup placement for large images (4000x4000 or more pixels)
-        if (stepFactor != 1) LOGGER.info("Using step factor of {} to speedup placement for large image with {}x{} pixel", stepFactor, dimension.width, dimension.height);
+//        if (stepFactor != 1)
+		LOGGER.info("Using step factor of {} to speedup placement for large image with {}x{} pixels", stepFactor, dimension.width, dimension.height);
 
         for (int r = 0; r < maxRadius; r += (2 * stepFactor) ) {
             for (int x = -r; x <= r; x += (1 * stepFactor) ) {
