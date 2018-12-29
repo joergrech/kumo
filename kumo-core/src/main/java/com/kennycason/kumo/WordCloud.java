5593eae96e3c436658be695705460b0d2a7ba4c9
Jörg Rech <joerg.rech@gmail.com>
2018-12-29 17:45:13 +0100

    Testing Wordclouds using color from shilouette

--- kumo-core/src/main/java/com/kennycason/kumo/WordCloud.java
+++ kumo-core/src/main/java/com/kennycason/kumo/WordCloud.java
@@ -214,7 +214,7 @@ public class WordCloud {
 					int centerX = word.getPosition().x + word.getBufferedImage().getWidth() / 2;
 					int centerY = word.getPosition().y + word.getBufferedImage().getHeight() / 2;
 					int underlyingColor = (int) 0xffff0000; // red test color
-//					underlyingColor = word.getBufferedImage().getRGB(centerX, centerY);
+					underlyingColor = word.getBufferedImage().getRGB(centerX, centerY);
 					int textColor = (int) 0xff000000; // assuming text color is black
 					BufferedImage recoloredWordImage = recolorImage(word.getBufferedImage(), textColor, underlyingColor);
 					graphics.drawImage(recoloredWordImage, word.getPosition().x, word.getPosition().y, null);
