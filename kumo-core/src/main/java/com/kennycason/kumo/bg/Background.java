5f0f5af5ef5b40ca0e3653f00a7efd4bc628f94b
Jörg Rech <joerg.rech@gmail.com>
2018-12-30 10:23:31 +0100

    Storing image with RGB data to identify underlying color

--- kumo-core/src/main/java/com/kennycason/kumo/bg/Background.java
+++ kumo-core/src/main/java/com/kennycason/kumo/bg/Background.java
@@ -2,9 +2,12 @@ package com.kennycason.kumo.bg;
 
 import com.kennycason.kumo.collide.Collidable;
 
+import java.awt.image.BufferedImage;
+
 /**
  * Created by kenny on 6/30/14.
  */
 public interface Background {
+    public BufferedImage bufferedImage = null; // NEW: to access underlying colors
     boolean isInBounds(Collidable collidable);
 }
